d06b210687c5ca282d6315005867de5c91b337e7
Cliff <cliff@dsanalytics.com>
2020-11-19 03:19:54 +0800

    additional codes

--- python_snippets.py
+++ python_snippets.py
@@ -1,4 +1,13 @@
 
+# correlation between two columns in pandas dataframe
+
+col1 = df['rrcConnectedUsers']
+col2 = df['cellDLThroughput']
+correlation = col1.corr(col2)
+
+print(correlation)
+
+
 # use fastparquet to read parquet files and convert into pandas dataframe
 
 import pandas as pd
@@ -6,5 +15,33 @@ from fastparquet import ParquetFile
 
 pf = ParquetFile('raw_cell_thpt')
 df = pf.to_pandas()
+
+# drop rows with null values
+
+df.dropna(inplace=True)
+
+# convert string to datetime
+
+df['dateTime'] = pd.to_datetime(df['dateTime'])
+
+# filters
+df2 = df.query(" userCount > 10 & weather == 'cloudy' ") # and condition
+df3 = df.query(" userCount <= 10 | weather == 'sunny' ") # OR condition
+df4 = df.query(" hour in ['01', '02']") # list comparison, exist in
+df5 = df.query(" hour not in ['01', '02']") # list comparison, do not exist in
+
+# count of lines
+df2.count() # df2.size() returns row count * column count 
+
+# sort
+df.sort_values(['month', 'dayType'])
+
+# unique
+df.drop_duplicates()
+
+# join dataframes
+pd.merge(df1, df2, on='key')
+pd.merge(df1, df2, on=['key1', 'key2'])
+pd.merge(df1, df2, on='col1', how='outer', indicator=True)
 
 
