c7e8f99a383f784acf006e262b8a849e40ab3a53
cliff <tanchcliff@protonmail.com>
2020-11-29 23:38:19 +0800

    re-organising...

--- python.py
+++ python.py
@@ -1,64 +1,52 @@
 # Python / Pandas codes
 
-
-correlation between two columns in pandas dataframe
-
-```
+# correlation between two columns in pandas dataframe
 col1 = df['col_name_1']
 col2 = df['col_name_2']
 correlation = col1.corr(col2)
-
 print(correlation)
-```
 
-use fastparquet to read parquet files and convert into pandas dataframe
-```
+
+# use fastparquet to read parquet files and convert into pandas dataframe
 import pandas as pd
 from fastparquet import ParquetFile
 
 pf = ParquetFile('raw_cell_thpt')
 df = pf.to_pandas()
-```
 
-drop rows with null values
-```
+
+# drop rows with null values
 df.dropna(inplace=True)
-```
 
-convert string to datetime
-```
+
+# convert string to datetime
 df['dateTime'] = pd.to_datetime(df['dateTime'])
-```
 
-simple filters
-```
+
+# simple filters
 df2 = df.query(" userCount > 10 & weather == 'cloudy' ") # and condition
 df3 = df.query(" userCount <= 10 | weather == 'sunny' ") # OR condition
 df4 = df.query(" hour in ['01', '02']") # list comparison, exist in
 df5 = df.query(" hour not in ['01', '02']") # list comparison, do not exist in
-```
 
-count of lines
-```
+
+# count of lines
 df.count()
 df.size() returns row count * column count
 df.shape[0]
-```
 
-sort
-```
+
+# sort
 df.sort_values(['month', 'dayType'])
-```
 
-unique
-```
+
+# unique
 df.drop_duplicates()
-```
 
-join dataframes
-```
+
+# join dataframes
 pd.merge(df1, df2, on='key')
 pd.merge(df1, df2, on=['key1', 'key2'])
 pd.merge(df1, df2, on='col1', how='outer', indicator=True)
-```
+
 
--- scala.scala
+++ scala.scala
@@ -1,10 +1,8 @@
-# Scala codes
+// Scala codes
 
-scala code to reformat numerical columns of dataframes.
+// scala code to reformat numerical columns of dataframes.
+// adds commas and removes exponential notations
 
-adds commas and removes exponential notations
-
-```
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{DoubleType, IntegerType, DecimalType, LongType}
 
@@ -13,24 +11,21 @@ def fmt(df:DataFrame, decimalPlaces:Int = 2): DataFrame = {
     case (acc, col) => acc.withColumn(col.name, format_number(df(col.name), decimalPlaces))
   }
 }
-```
 
-Custom defined datasets might throw schema errors during unit test case's assertion comparison operation against computed values 
 
-Below is a spark scala method to set dataframe columns to be nullable, it sets nullable property for each column of the data frame
+// Custom defined datasets might throw schema errors during unit test case's assertion comparison operation against computed values 
+// Below is a spark scala method to set dataframe columns to be nullable, it sets nullable property for each column of the data frame
 
-```
 def setNullableStateForAllColumns(df: DataFrame, nullable: Boolean): DataFrame = {
   df.sqlContext.createDataFrame(df.rdd, StructType(df.schema.map(_.copy(nullable = nullable))))
 }
 
-```
 
 
-when using python based fastparquet and pandas libaries in jupyter notebooks, you might encounter errors regarding the lack of meta data
 
-you can use the below scala snippet to save the meta data file and enable parsing by fastparquet
-```
+// when using python based fastparquet and pandas libaries in jupyter notebooks, you might encounter errors regarding the lack of meta data
+// you can use the below scala snippet to save the meta data file and enable parsing by fastparquet
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.parquet.hadoop.{ParquetFileReader, ParquetFileWriter}
@@ -43,8 +38,5 @@ val uri = new URI(target_path)
 val fs = FileSystem.get(uri, conf)
 val footers = ParquetFileReader.readAllFootersInParallel(conf, fs.getFileStatus(new Path(target_path)))
 ParquetFileWriter.writeMetadataFile(conf, new Path(target_path), footers)
-
-```
-
 
 
