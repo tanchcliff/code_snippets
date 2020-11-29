0ef9055b3ebf3b73a49bf86f75dde6e98efac3ed
cliff <tanchcliff@protonmail.com>
2020-11-30 00:06:29 +0800

    reorganising...

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Code Snippets
 
-some code snippets in scala and python
+repo of code snippets.
+mainly scala and python stuff.
+
+### Scala
+
+contains snippets to do different operations:
+
+  - reformat numerical columns of dataframes for display (remove exponentials)
+  - set nullable property of columns in dataset
+  - save meta data of parquet files
+  - tokenisation of dataset (instead of using hashing algo which results in hash collisions when applied on a large dataset)
+  - unpivot a dataframe
+  - timer class for timing code execution durations
--- scala.scala
+++ scala.scala
@@ -40,3 +40,80 @@ val footers = ParquetFileReader.readAllFootersInParallel(conf, fs.getFileStatus(
 ParquetFileWriter.writeMetadataFile(conf, new Path(target_path), footers)
 
 
+// tokenisation code snippet for managign sensitive PII values
+// start with primary keys that exist as foreign key columns in other tables such as subscriber Ids
+// code is straightforward to follow. we use monotonically_increasing_id() to create new indexes to replace the original values
+
+val subscriberPath = ""
+val subs = spark.read.parquet(subscriberPath)
+val targetCol = "subId"
+val subId_map = subs.select(targetCol)
+                    .distinct
+                    .withColumn("new_" + targetCol,
+                      concat(lit(targetCol + "_"), (monotonically_increasing_id().cast(StringType))))
+
+subId_map.write.parquet("tmp/subId_map")
+val subs2 = subs.join(subId_map, Seq(targetCol), "left")
+	.na.fill("unmatched", Seq("new_" + targetCol))
+	.withColumn(targetCol, col("new_" + targetCol))
+        .drop("new_" + targetCol)
+
+
+// unpivot a dataframe
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.expr
+
+/*
+ * given input dataframe
+ * | t   | header1 | header2 | header3 |
+ * |-----|---------|---------|---------|
+ * |test |   20.0  |   30.0  |   40.0  |
+ *
+ * function will return dataframe
+ * | t   | col0    | col1  |
+ * |-----|---------|-------|
+ * |test | header1 | 20.0  |
+ * |test | header2 | 30.0  |
+ * |test | header3 | 40.0  |
+ *
+ * @param df input dataframe
+ * @param headers columns to transpose. They must be of same types e.g. String and Double columns should not be mixed together
+ * @param unpivotKey Optional column to retain. in the e.g. above, t will be the unpivotKey
+ * @return unpivoted dataframe with col0 and col1 representing headers and values respectively
+ */
+def unpivot(
+  df: DataFrame,
+  headers: List[String],
+  unpivotKey: Option[String] = None): DataFrame = {
+
+  val headersLen = headers.length.toString
+  // stack expression example: stack(2, 'c1', c1 , 'c2', c2)
+  val stackExpr = s"stack(${headersLen}, ${headers.map(h => s"'${h}', ${h}").mkString(", ")})"
+
+  unpivotKey match {
+    case Some(colName) => df.select(col(colName), expr(stackExpr))
+    case None => df.select(expr(stackExpr))
+  }
+}
+
+
+
+// timer to measure code execution duration
+
+class Timer(startTime: Long = System.currentTimeMillis()) extends Serializable {
+
+  private def currentTime: Long = System.currentTimeMillis()
+
+  def stop: Long = currentTime - startTime
+
+  def getTimeElapsedStr:String = "%1.2f".format(this.stop / 1000.0 / 60.0) + " minutes" // fmt two d.p.
+
+}
+
+// assuming we already have a logger initialised
+log.info(s"${appName} job begin!")
+val timer = new Timer()
+// process some code
+log.info(s"${appName} completed. Duration: " + timer.getTimeElapsedStr)
